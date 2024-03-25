9d77ba62a7c150e7d9ac6e20f235a5f1e2310035
rpdeshaies <rpdeshaies@gmail.com>
2024-03-25 15:11:43 -0400

    feat: github

--- sites/zzap.dev/docs/src/Root.tsx
+++ sites/zzap.dev/docs/src/Root.tsx
@@ -65,6 +65,15 @@ export function Root(props: { children: React.ReactNode; content: string }) {
             </li>
             <li>
               <a
+                className="tw-flex tw-cursor-pointer contrast"
+                href="https://github.com/zzapdotdev/zzap"
+                target="_blank"
+              >
+                <GitHubIcon></GitHubIcon>
+              </a>
+            </li>
+            <li>
+              <a
                 className="tw-flex tw-cursor-pointer contrast "
                 onClick={(e) => {
                   e.preventDefault();
@@ -124,6 +133,26 @@ function MoonIcon(props: { className?: string }) {
         strokeLinejoin="round"
         d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
       />
+    </svg>
+  );
+}
+
+function GitHubIcon(props: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      class={clsx("tw-h-6 tw-w-6", props.className)}
+    >
+      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+      <path d="M9 18c-4.51 2-5-2-7-2" />
     </svg>
   );
 }
