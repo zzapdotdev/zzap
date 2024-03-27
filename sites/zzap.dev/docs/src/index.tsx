2a1aadecbff5f0823d1f9ed6791d85b16335b1cd
rpdeshaies <rpdeshaies@gmail.com>
2024-03-27 16:43:16 -0400

    fix: content width

--- sites/zzap.dev/docs/src/index.tsx
+++ sites/zzap.dev/docs/src/index.tsx
@@ -220,13 +220,14 @@ export default function App(props: { page: PageType<"home-page"> }) {
 
     const html = props.page.type === "markdown" && props.page.html;
     return (
-      <div>
+      <div
+        className={clsx({
+          "tw-w-[100%]": isHomePage,
+          "tw-w-[100%] lg:tw-w-[75%]": !isHomePage,
+        })}
+      >
         {html && (
           <div
-            className={clsx({
-              "tw-w-[100%]": isHomePage,
-              "tw-w-[100%] lg:tw-w-[75%]": !isHomePage,
-            })}
             dangerouslySetInnerHTML={{
               __html: html,
             }}
