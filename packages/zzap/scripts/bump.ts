fe2388b2cb6c9cda9d74a498ab4186009b43efa3
rpdeshaies <rpdeshaies@gmail.com>
2024-03-28 15:06:07 -0400

    fix: bump

--- packages/zzap/scripts/bump.ts
+++ packages/zzap/scripts/bump.ts
@@ -26,6 +26,6 @@ await $`git push origin v${newVersion}`;
 
 logger.log("Committing");
 await $`git add .`;
-await $`git commit -m "chore: bump package.json to ${newVersion}"`;
+await $`git commit -m "chore: bump package.json version to v${newVersion}"`;
 logger.log("Pushing");
 await $`git push`;
