62e94467813dd7bc419a8708662dea93d10e8f3a
rpdeshaies <rpdeshaies@gmail.com>
2024-03-28 14:38:05 -0400

    feat: autotag

---
+++ packages/zzap/scripts/bump.ts
@@ -0,0 +1,31 @@
+import { $ } from "bun";
+import { getLogger } from "../src/domains/logging/getLogger";
+
+const logger = getLogger("distribute");
+
+const packageJson = await Bun.file("./package.json").json();
+const [major, minor, patch] = packageJson.version.split(".");
+const newVersion = `${major}.${minor}.${parseInt(patch) + 1}`;
+
+logger.log(`Current version: "${packageJson.version}"`);
+logger.log(`Bumping to ${newVersion}`);
+
+logger.log("Updating package.json");
+packageJson.version = newVersion;
+await Bun.write("./package.json", JSON.stringify(packageJson, null, 2));
+
+logger.log("Tagging");
+await $`git tag -a v${newVersion} -m "v${newVersion}"`;
+
+logger.log("Configuring Git");
+await $`git config user.name github-actions`;
+await $`git config user.email`;
+
+logger.log("Pushing tag");
+await $`git push origin v${newVersion}`;
+
+logger.log("Committing");
+await $`git add .`;
+await $`git commit -m "generated"`;
+logger.log("Pushing");
+await $`git push`;
