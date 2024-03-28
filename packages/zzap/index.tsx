f2b796bf002dfd921b051175a289ac65de38e12b
rpdeshaies <rpdeshaies@gmail.com>
2024-03-28 14:19:32 -0400

    rename

--- packages/zzap/index.tsx
+++ packages/zzap/index.tsx
@@ -1,5 +1,5 @@
-import { ZzapPluginPicoCSS } from "./plugins/ZzapPluginPicoCSS";
-import { ZzapPluginTailwind } from "./plugins/ZzapPluginTailwind";
+import { ZzapPluginPicoCSS } from "./plugins/pico";
+import { ZzapPluginTailwind } from "./plugins/tailwind";
 
 export { defineConfig } from "./src/domains/config/zzapConfigSchema";
 export { definePlugin } from "./src/domains/plugin/definePlugin";
